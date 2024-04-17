bc5aea118f1d54de4175f2b16b584b7baad237dc
Jay <S1555517@student.mcckc.edu>
2024-04-17 12:53:18 -0500

    Initial commit

---
+++ src/main/kotlin/net/hauc3/hauc3raft/HauC3raft.kt
@@ -0,0 +1,10 @@
+package net.hauc3.hauc3raft
+
+import org.bukkit.plugin.java.JavaPlugin
+
+@Suppress("unused")
+class HauC3raft: JavaPlugin() {
+    override fun onEnable() {
+        logger.info("Hello, world!")
+    }
+}
